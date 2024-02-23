2edda4d52ecd56ec853f2699bdd5d25834d6369d
MonkeyArya <53228785+MonkeyArya@users.noreply.github.com>
2024-02-23 04:42:14 -0500

    Got the people to move in place

--- people.gd
+++ people.gd
@@ -1,15 +1,16 @@
 extends Area2D
 @export var enemy_scene: PackedScene
 var entered = 0; 
+var dir = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$WalkTime.start() # Replace with function body.
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	position.x += dir * 0.25
 
 
 func _on_body_entered(body):
@@ -25,3 +26,9 @@ func _on_body_entered(body):
 
 func _on_body_exited(body):
 	print("exited") # Replace with function body.
+
+
+func _on_walk_time_timeout():
+	dir = dir * -1
+	$AnimatedSprite2D.flip_h = dir > 0
+	$WalkTime.start() # Replace with function body.
