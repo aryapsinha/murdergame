9ebd672662f82bca874bd19c5594fca51bb3fc27
MonkeyArya <53228785+MonkeyArya@users.noreply.github.com>
2024-02-23 04:32:33 -0500

    detection functionality

--- people.gd
+++ people.gd
@@ -1,11 +1,27 @@
 extends Area2D
 @export var enemy_scene: PackedScene
+var entered = 0; 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body):
+	#print("entered")
+	
+	
+	if(body.name == "Criminal"):
+		body.hide()
+		#emit game over signal
+		#Replace with function body.
+	
+
+
+func _on_body_exited(body):
+	print("exited") # Replace with function body.
