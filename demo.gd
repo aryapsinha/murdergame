a3ec94fa598f12a7b17e01f9b341c25b3e6d6e02
MonkeyArya <53228785+MonkeyArya@users.noreply.github.com>
2024-02-24 11:59:50 -0500

    Game Over, started knife

--- demo.gd
+++ demo.gd
@@ -4,13 +4,16 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	 # Replace with function body.
+	#$SubViewportContainer/EndScreen/LabelVis/Label.show()
+	#$SubViewportContainer/EndScreen._on_people_gameover()
+	
 	var numPeople = randi() % 4 + 1
 	$Criminal/MainCam.make_current() #Change this if a start scene is added
 	for i in numPeople:
 		var enemy_scene_copy = enemy_scene.instantiate()
-		var x = randi() % 150 + 50
+		var x = randi() % 150 + 350
 		print(x)
-		var y = randi()% 100 - 100
+		var y = randi()% 100 + 175
 		print(y)
 		enemy_scene_copy.position = Vector2(x, y)
 		#var hud = $EndScreen.new()
